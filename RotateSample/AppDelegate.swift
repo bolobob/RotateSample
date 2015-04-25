18315caf8569417a54415d41ba1dd8853614a859
bolobob <aokishinichi@gmail.com>
2015-04-25 22:09:52 +0900

    ストーリボードを使わない

--- RotateSample/AppDelegate.swift
+++ RotateSample/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
 
         return true
     }

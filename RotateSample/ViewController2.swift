0bbf455402db110ed99cde6ac2adb73c188cfb6e
bolobob <aokishinichi@gmail.com>
2015-04-25 23:28:00 +0900

    リネーム

--- RotateSample/ViewController2.swift
+++ RotateSample/ViewController2.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController2: UIViewController {
 
     let label = UILabel()
-    let buttonToVC2 = UIButton()
+    let buttonToBack = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,22 +26,22 @@ class ViewController2: UIViewController {
         ]
         view.addConstraints(constraints)
 
-        buttonToVC2.setTitle("戻る", forState: .Normal)
-        buttonToVC2.setTitleColor(UIColor.blackColor(), forState: .Normal)
-        buttonToVC2.layer.borderWidth = 1
-        buttonToVC2.layer.cornerRadius = 4
-        buttonToVC2.layer.borderColor = UIColor.grayColor().CGColor
-        buttonToVC2.titleEdgeInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
-        buttonToVC2.sizeToFit()
-        view.addSubview(buttonToVC2)
-        buttonToVC2.setTranslatesAutoresizingMaskIntoConstraints(false)
+        buttonToBack.setTitle("戻る", forState: .Normal)
+        buttonToBack.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        buttonToBack.layer.borderWidth = 1
+        buttonToBack.layer.cornerRadius = 4
+        buttonToBack.layer.borderColor = UIColor.grayColor().CGColor
+        buttonToBack.titleEdgeInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        buttonToBack.sizeToFit()
+        view.addSubview(buttonToBack)
+        buttonToBack.setTranslatesAutoresizingMaskIntoConstraints(false)
         view.addConstraints([
-            NSLayoutConstraint(item: buttonToVC2, attribute: .Width, relatedBy: .Equal, toItem: view, attribute: .Width, multiplier: 0.5, constant: 0),
-            NSLayoutConstraint(item: buttonToVC2, attribute: .Bottom, relatedBy: .Equal, toItem: view, attribute: .Bottom, multiplier: 1.0, constant: -20),
-            NSLayoutConstraint(item: buttonToVC2, attribute: .CenterX, relatedBy: .Equal, toItem: view, attribute: .CenterX, multiplier: 1.0, constant: 0),
+            NSLayoutConstraint(item: buttonToBack, attribute: .Width, relatedBy: .Equal, toItem: view, attribute: .Width, multiplier: 0.5, constant: 0),
+            NSLayoutConstraint(item: buttonToBack, attribute: .Bottom, relatedBy: .Equal, toItem: view, attribute: .Bottom, multiplier: 1.0, constant: -20),
+            NSLayoutConstraint(item: buttonToBack, attribute: .CenterX, relatedBy: .Equal, toItem: view, attribute: .CenterX, multiplier: 1.0, constant: 0),
             ])
 
-        buttonToVC2.addTarget(self, action: "back:", forControlEvents: UIControlEvents.TouchUpInside)
+        buttonToBack.addTarget(self, action: "back:", forControlEvents: UIControlEvents.TouchUpInside)
     }
 
     func back(sender: UIButton) {

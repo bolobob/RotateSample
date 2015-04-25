d6cd6ad5dc2d0d21ba5a3bfcd7b480817b1fc88a
bolobob <aokishinichi@gmail.com>
2015-04-25 22:46:54 +0900

    ボタンの追加

--- RotateSample/ViewController.swift
+++ RotateSample/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController {
 
     let label: UILabel = UILabel()
+    let buttonToVC2: UIButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +25,21 @@ class ViewController: UIViewController {
             NSLayoutConstraint(item: label, attribute: .CenterY, relatedBy: .Equal, toItem: view, attribute: .CenterY, multiplier: 1.0, constant: 0),
         ]
         view.addConstraints(constraints)
+
+        buttonToVC2.setTitle("VC2へ", forState: .Normal)
+        buttonToVC2.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        buttonToVC2.layer.borderWidth = 1
+        buttonToVC2.layer.cornerRadius = 4
+        buttonToVC2.layer.borderColor = UIColor.grayColor().CGColor
+        buttonToVC2.titleEdgeInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        buttonToVC2.sizeToFit()
+        view.addSubview(buttonToVC2)
+        buttonToVC2.setTranslatesAutoresizingMaskIntoConstraints(false)
+        view.addConstraints([
+            NSLayoutConstraint(item: buttonToVC2, attribute: .Width, relatedBy: .Equal, toItem: view, attribute: .Width, multiplier: 0.5, constant: 0),
+            NSLayoutConstraint(item: buttonToVC2, attribute: .Bottom, relatedBy: .Equal, toItem: view, attribute: .Bottom, multiplier: 1.0, constant: -20),
+            NSLayoutConstraint(item: buttonToVC2, attribute: .CenterX, relatedBy: .Equal, toItem: view, attribute: .CenterX, multiplier: 1.0, constant: 0),
+            ])
     }
 
     override func didReceiveMemoryWarning() {

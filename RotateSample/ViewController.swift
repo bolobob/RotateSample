d00110c785226b7447767e2f08ea84f57482816b
bolobob <aokishinichi@gmail.com>
2015-04-25 22:11:05 +0900

    デバイスを回転した時に分かりやすいように文字列を配置

--- RotateSample/ViewController.swift
+++ RotateSample/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let label: UILabel = UILabel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.whiteColor()
+        label.text = "RotateSample"
+        label.sizeToFit()
+        view.addSubview(label)
+        label.setTranslatesAutoresizingMaskIntoConstraints(false)
+        let constraints = [
+            NSLayoutConstraint(item: label, attribute: .CenterX, relatedBy: .Equal, toItem: view, attribute: .CenterX, multiplier: 1.0, constant: 0),
+            NSLayoutConstraint(item: label, attribute: .CenterY, relatedBy: .Equal, toItem: view, attribute: .CenterY, multiplier: 1.0, constant: 0),
+        ]
+        view.addConstraints(constraints)
     }
 
     override func didReceiveMemoryWarning() {

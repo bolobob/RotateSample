ba53c9f72fd156b4943f36ef5ef0f95b6c745a5e
bolobob <aokishinichi@gmail.com>
2015-04-25 23:24:22 +0900

    ViewControllerへの遷移ボタンを作成

--- RotateSample/ViewController2.swift
+++ RotateSample/ViewController2.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController2: UIViewController {
 
     let label = UILabel()
+    let buttonToVC2 = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +25,27 @@ class ViewController2: UIViewController {
             NSLayoutConstraint(item: label, attribute: .CenterY, relatedBy: .Equal, toItem: view, attribute: .CenterY, multiplier: 1.0, constant: 0),
         ]
         view.addConstraints(constraints)
+
+        buttonToVC2.setTitle("戻る", forState: .Normal)
+        buttonToVC2.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        buttonToVC2.layer.borderWidth = 1
+        buttonToVC2.layer.cornerRadius = 4
+        buttonToVC2.layer.borderColor = UIColor.grayColor().CGColor
+        buttonToVC2.titleEdgeInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        buttonToVC2.sizeToFit()
+        view.addSubview(buttonToVC2)
+        buttonToVC2.setTranslatesAutoresizingMaskIntoConstraints(false)
+        view.addConstraints([
+            NSLayoutConstraint(item: buttonToVC2, attribute: .Width, relatedBy: .Equal, toItem: view, attribute: .Width, multiplier: 0.5, constant: 0),
+            NSLayoutConstraint(item: buttonToVC2, attribute: .Bottom, relatedBy: .Equal, toItem: view, attribute: .Bottom, multiplier: 1.0, constant: -20),
+            NSLayoutConstraint(item: buttonToVC2, attribute: .CenterX, relatedBy: .Equal, toItem: view, attribute: .CenterX, multiplier: 1.0, constant: 0),
+            ])
+
+        buttonToVC2.addTarget(self, action: "back:", forControlEvents: UIControlEvents.TouchUpInside)
+    }
+
+    func back(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
     }
 
 }
